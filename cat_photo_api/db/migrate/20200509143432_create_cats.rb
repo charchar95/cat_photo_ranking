6843ed7f90f92c6ee4c9118b235a9f88573ed266
Charlie Humphrey <charliehumphrey@Charlies-MacBook-Pro.local>
2020-05-09 07:50:55 -0700

    database seeded

---
+++ cat_photo_api/db/migrate/20200509143432_create_cats.rb
@@ -0,0 +1,11 @@
+class CreateCats < ActiveRecord::Migration[6.0]
+  def change
+    create_table :cats do |t|
+      t.string :name
+      t.string :img
+      t.string :comments
+
+      t.timestamps
+    end
+  end
+end

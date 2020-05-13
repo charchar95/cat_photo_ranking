712f98d9e7004a2fe910f1908a3fdf7f0d8f11a6
Charlie Humphrey <charliehumphrey@Charlies-MacBook-Pro.local>
2020-05-12 17:50:53 -0700

    updated cors for heroku

--- cat_photo_api/config/initializers/cors.rb
+++ cat_photo_api/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins  'https://cat-photo-ranking-api.herokuapp.com',
+    origins  'https://cat-photo-ranking-api.herokuapp.com', 'https://cat-photo-ranking-client.herokuapp.com',
 
     resource '*',
       headers: :any,

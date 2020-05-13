24247665729f38d37634bbf103e69680656e504a
Charlie Humphrey <charliehumphrey@Charlies-MacBook-Pro.local>
2020-05-12 17:54:11 -0700

    attempt #52 to solve heroku cors issue

--- cat_photo_api/config/initializers/cors.rb
+++ cat_photo_api/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins  '*',
+    origins  'https://cat-photo-ranking-client.herokuapp.com',
 
     resource '*',
       headers: :any,
